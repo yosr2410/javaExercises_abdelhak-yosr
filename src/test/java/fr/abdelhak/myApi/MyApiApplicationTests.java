1ecc3a370270cbac79b7a4e13536b93640e7a3ff
yosr <yossser27@gmail.com>
2023-06-21 13:42:14 +0200

    Spring Boot

---
+++ src/test/java/fr/abdelhak/myApi/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.abdelhak.myApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
